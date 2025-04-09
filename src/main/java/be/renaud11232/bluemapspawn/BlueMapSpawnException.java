aad032c0962e3d45432c9a1a9d53b7990d47e176
Renaud11232 <gaspardrenaud@hotmail.com>
2025-04-09 21:17:27 +0200

    added custom exception

---
+++ src/main/java/be/renaud11232/bluemapspawn/BlueMapSpawnException.java
@@ -0,0 +1,7 @@
+package be.renaud11232.bluemapspawn;
+
+public class BlueMapSpawnException extends RuntimeException {
+    public BlueMapSpawnException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
